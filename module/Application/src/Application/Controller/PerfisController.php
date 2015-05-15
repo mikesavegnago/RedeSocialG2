f89cca3f484f82a8ccb9de7cd28c3c17f9f3c975
anapbinda <anapaulabinda@gmail.com>
2015-05-14 21:44:40 -0300

    Criado todos os controllers

---
+++ module/Application/src/Application/Controller/PerfisController.php
@@ -0,0 +1,47 @@
+<?php
+namespace Application\Controller;
+
+use Core\Controller\ActionController as ActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+* Controller Perfis
+*
+* @category Application
+* @package  Controller
+* @author   Ana Paula Binda <anapaulasif@unochapeco.edu.br>
+* @license  Copyright <http://www.softwarecontracts.net/p05_copyright_patent_software.htm>
+* @link     localhost
+*/
+class PerfisController extends ActionController
+{
+    /**
+    *Função index
+    *
+    *@return void
+    */
+    public function indexAction()
+    {
+        
+        return new ViewModel();
+    }
+    
+    /**
+    *Função save
+    *
+    *@return void
+    */
+    public function saveAction()
+    {
+       
+    }
+
+    /**
+    *Função delete
+    *
+    *@return void
+    */
+    public function deleteAction()
+    {
+    
+    }
