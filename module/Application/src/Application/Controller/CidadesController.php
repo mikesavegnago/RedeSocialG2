8f40c64390559c8b99b2078c444a31ca926b2c7c
Lindzionei Zanin <lindzioneizanin@unochapeco.edu.br>
2015-05-14 21:41:28 -0300

    resolvendo bugs

--- module/Application/src/Application/Controller/CidadesController.php
+++ module/Application/src/Application/Controller/CidadesController.php
@@ -1,25 +1,16 @@
 <?php
-<<<<<<< HEAD
 namespace Application\Controller;
 
 use Core\Controller\ActionController as ActionController;
 use Application\Form\Cidade as Form;
-=======
-namespace Admin\Controller;
-
-use Core\Controller\ActionController as ActionController;
-use Admin\Form\Cidade as Form;
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
 use Zend\View\Model\ViewModel;
 
 /**
 * Controller Cidades
 *
-<<<<<<< HEAD
+
 * @category Application
-=======
-* @category Admin
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
+ * 
 * @package  Controller
 * @author   Cezar Junior de Souza <cezar08@unochapeco.edu.br>
 * @license  Copyright <http://www.softwarecontracts.net/p05_copyright_patent_software.htm>
@@ -34,11 +25,8 @@ class CidadesController extends ActionController
     */
     public function indexAction()
     {
-<<<<<<< HEAD
+
         var_dump($this->getService('Application\Service\Cidade')->fetchAll());
-=======
-        var_dump($this->getService('Admin\Service\Cidade')->fetchAll());
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
         exit;
     }
 
@@ -52,38 +40,22 @@ class CidadesController extends ActionController
         $form = new Form($this->getObjectManager());
         $request = $this->getRequest();
         if ($request->isPost()) {            
-<<<<<<< HEAD
             $form->setInputFilter(\Application\Model\Cidade::getInputFilter());            
-=======
-            $form->setInputFilter(\Admin\Model\Cidade::getInputFilter());            
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
             $form->setData($request->getPost());            
             if ( $form->isValid() ) {                
                 $values = $form->getData();
                 try {
-<<<<<<< HEAD
                     $uf = $this->getService('Application\Service\Cidade')->save($values);
-=======
-                    $uf = $this->getService('Admin\Service\Cidade')->save($values);
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
                 } catch(\Exception $e) {
                     echo $e->getMessage(); 
                     exit;
                 }
-<<<<<<< HEAD
                 return $this->redirect()->toUrl('/Application/cidades');    
-=======
-                return $this->redirect()->toUrl('/admin/cidades');    
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
             }                    
         }
         $id = (int) $this->params()->fromRoute('id', 0);
         if ($id > 0) {
-<<<<<<< HEAD
             $cidade = $this->getService('Application\Service\Cidade')->find($id);            
-=======
-            $cidade = $this->getService('Admin\Service\Cidade')->find($id);            
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
             $form->bind($cidade);
         }
 
@@ -99,20 +71,11 @@ class CidadesController extends ActionController
     {
         $id = (int) $this->params()->fromRoute('id', 0);
         try{
-<<<<<<< HEAD
             $this->getService('Application\Service\Cidade')->delete($id);
-=======
-            $this->getService('Admin\Service\Cidade')->delete($id);
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
         }catch(\Exception $e){
             echo $e->getMessage();
             exit;
         }
-
-<<<<<<< HEAD
         return $this->redirect()->toUrl('/Application/cidades');
-=======
-        return $this->redirect()->toUrl('/admin/cidades');
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
     }
 }
--- module/Application/src/Application/Controller/UfsController.php
+++ module/Application/src/Application/Controller/UfsController.php
@@ -1,25 +1,15 @@
 <?php
-<<<<<<< HEAD
+
 namespace Application\Controller;
 
 use Core\Controller\ActionController as ActionController;
 use Application\Form\Uf as Form;
-=======
-namespace Admin\Controller;
-
-use Core\Controller\ActionController as ActionController;
-use Admin\Form\Uf as Form;
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
 use Zend\View\Model\ViewModel;
 
 /**
 * Controller Ufs
-*
-<<<<<<< HEAD
+* 
 * @category Application
-=======
-* @category Admin
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
 * @package  Controller
 * @author   Cezar Junior de Souza <cezar08@unochapeco.edu.br>
 * @license  Copyright <http://www.softwarecontracts.net/p05_copyright_patent_software.htm>
@@ -34,11 +24,7 @@ class UfsController extends ActionController
     */
     public function indexAction()
     {
-<<<<<<< HEAD
         $ufs = $this->getService('Application\Service\Uf')->fetchAll();
-=======
-        $ufs = $this->getService('Admin\Service\Uf')->fetchAll();
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
 
         return new ViewModel(array('ufs' => $ufs));
     }
@@ -53,20 +39,14 @@ class UfsController extends ActionController
         $form = new Form();
         $request = $this->getRequest();
         if ($request->isPost()) {
-<<<<<<< HEAD
             $form->setInputFilter(\Application\Model\Uf::getInputFilter());
-=======
-            $form->setInputFilter(\Admin\Model\Uf::getInputFilter());
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
             $form->setData($request->getPost());
             if ($form->isValid()) {
                 $values = $form->getData();
                 try{
-<<<<<<< HEAD
+
                     $uf = $this->getService('Application\Service\Uf')->save($values);
-=======
-                    $uf = $this->getService('Admin\Service\Uf')->save($values);
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
+
                 }catch(\Exception $e){
                     echo $e->getMessage(); 
                     exit;
@@ -76,11 +56,7 @@ class UfsController extends ActionController
         }
         $id = (int) $this->params()->fromRoute('id', 0);
         if ($id > 0) {
-<<<<<<< HEAD
             $uf = $this->getService('Application\Service\Uf')->find($id);
-=======
-            $uf = $this->getService('Admin\Service\Uf')->find($id);
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
             $form->bind($uf);
         }
 
@@ -96,13 +72,9 @@ class UfsController extends ActionController
     {
         $id = (int) $this->params()->fromRoute('id', 0);
         try {
-<<<<<<< HEAD
             $this->getService('Application\Service\Uf')->delete($id);
-=======
-            $this->getService('Admin\Service\Uf')->delete($id);
->>>>>>> parent of 456f75b... Revert "UF e Cidade"
         } catch(\Exception $e) {
-            echo $e->getMessage();
+          echo $e->getMessage();
         }
 
         return $this->redirect()->toUrl('/admin/ufs');
