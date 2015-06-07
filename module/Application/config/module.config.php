f2efc2b5e0b29313428c9c3ef9bec9f86d3c1124
Paulo Cella <paulocella@unochapeco.edu.br>
2015-06-07 16:24:41 -0300

    substituido o module.config pelo do blog, e adicionado  os invocables

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -1,88 +1,9 @@
+
+
 <?php
 
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
+// module/Application/conﬁg/module.config.php:
 return array(
-    'router' => array(
-        'routes' => array(
-            'home' => array(
-                'type' => 'Zend\Mvc\Router\Http\Literal',
-                'options' => array(
-                    'route' => '/',
-                    'defaults' => array(
-                        'controller' => 'Application\Controller\Login',
-                        'action' => 'index',
-                    ),
-                ),
-            ),
-            // The following is a route to simplify getting started creating
-            // new controllers and actions without needing to create a new
-            // module. Simply drop new controllers in, and you can access them
-            // using the path /application/:controller/:action
-            'application' => array(
-                'type' => 'Literal',
-                'options' => array(
-                    'route' => '/application',
-                    'defaults' => array(
-                        '__NAMESPACE__' => 'Application\Controller',
-                        'controller' => 'Index',
-                        'action' => 'index',
-                    ),
-                ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type' => 'Segment',
-                        'options' => array(
-                            'route' => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
-            ),
-        ),
-    ),
-    'doctrine' => array(
-        'driver' => array(
-            'application_entities' => array(
-                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
-                'cache' => 'array',
-                'paths' => array(__DIR__ . '/../src/Application/Entity')
-            ),
-            'orm_default' => array(
-                'drivers' => array(
-                    'Application\Entity' => 'application_entities'
-                )
-            ))),
-    'service_manager' => array(
-        'abstract_factories' => array(
-            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
-            'Zend\Log\LoggerAbstractServiceFactory',
-        ),
-        'aliases' => array(
-            'translator' => 'MvcTranslator',
-        ),
-    ),
-    'translator' => array(
-        'locale' => 'en_US',
-        'translation_file_patterns' => array(
-            array(
-                'type' => 'gettext',
-                'base_dir' => __DIR__ . '/../language',
-                'pattern' => '%s.mo',
-            ),
-        ),
-    ),
     'controllers' => array(
         'invokables' => array(
             'Application\Controller\Login' => 'Application\Controller\LoginController',
@@ -92,7 +13,6 @@ return array(
             'Application\Controller\Imagens' => 'Application\Controller\ImagensController',
             'Application\Controller\Eventos' => 'Application\Controller\EventosController',
             'Application\Controller\Usuarios' => 'Application\Controller\UsuariosController',
-            
             'Application\Controller\Comentarios' => 'Application\Controller\ComentariosController',
             'Application\Controller\Cidades' => 'Application\Controller\CidadesController',
             'Application\Controller\Ufs' => 'Application\Controller\UfsController',
@@ -101,27 +21,122 @@ return array(
             'Application\Controller\Enderecos' => 'Application\Controller\EnderecosController',
         ),
     ),
-    'view_manager' => array(
-        'display_not_found_reason' => true,
-        'display_exceptions' => true,
-        'doctype' => 'HTML5',
-        'not_found_template' => 'error/404',
-        'exception_template' => 'error/index',
-        'template_map' => array(
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'application/login/index' => __DIR__ . '/../view/application/login/index.phtml',
-            'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
-        ),
-        'template_path_stack' => array(
-            __DIR__ . '/../view',
-        ),
-    ),
-    // Placeholder for console routes
-    'console' => array(
-        'router' => array(
-            'routes' => array(
+    //Configuração doctrine
+    'doctrine' => array(
+        'authentication' => array(
+            'orm_default' => array(
+                'object_manager' => 'Doctrine\ORM\EntityManager',
+                'identity_class' => 'Application\Entity\Usuario',
+                'identity_property' => 'login',
+                'credential_property' => 'senha',
             ),
+            ),
+        'driver' => array(
+            'application_entities' => array(
+                'class' =>'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/Application/Entity'
+                      )
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'Application\Entity' => 'application_entities'
+                )
+            ), 
+        )
+    ),
+    //*********************************************************
+
+    'router' => array(
+        'routes' => array(
+            'application' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/application',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Application\Controller',
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                        'module'        => 'application'
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/[:controller[/:action]]',
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                        'child_routes' => array( //permite mandar dados pela url 
+                            'wildcard' => array(
+                                'type' => 'Wildcard'
+                            ),
+                        ),
+                    ),
+                    
+                ),
+            ),
+            'index_paginacao' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/[page/:page]',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'Main\Controller',
+                        'controller' => 'Index',
+                        'action' => 'index',
+                        'module' => 'main',
+                        'page' => 1,
+                    ),
+                ),
+            ), 
         ),
     ),
+    'service_manager' => array(
+        'factories' => array(
+            'Session' => function ($sm) {
+                return new Zend\Session\Container('SessionApplication');
+            },
+        )
+    ),
+    
+//    'service_manager' => array(
+//        'factories' => array(
+//            'Session' => function ($sm) {
+//                return new Zend\Session\Container('SessionApplication');
+//            },
+//        )
+//    ),
+    
+                    
+//                    
+//    'service_manager' => array(
+//       'factories' => array(
+//           'Session' => function ($sm) {
+//               return new Zend\Session\Container('SessionApplication');
+//           },
+//       ),
+//       'abstract_factories' => array(
+//           'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+//           'Zend\Log\LoggerAbstractServiceFactory',
+//       ),
+//       'aliases' => array(
+//           'translator' => 'MvcTranslator',
+//       ),
+//   ),
+//                    
+                    
+                    
+
+    'view_manager' => array( 
+        'template_path_stack' => array(
+            'application' => __DIR__ . '/../view',
+        ),
+    ),
+   
 );
