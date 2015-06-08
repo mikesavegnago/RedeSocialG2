46298813e4badefa359b91a2e485e7e25612e9cb
Paulo Cella <paulocella@unochapeco.edu.br>
2015-06-08 09:15:47 -0300

    Ajustados para application

--- module/Application/src/Application/Controller/EnderecosController.php
+++ module/Application/src/Application/Controller/EnderecosController.php
@@ -15,7 +15,8 @@ use Zend\View\Model\ViewModel;
 
 class EnderecosController extends AbstractActionController {
 
-    public function indexAction() {
+    public function indexAction() 
+    {
         
     }
 
@@ -26,12 +27,58 @@ class EnderecosController extends AbstractActionController {
         $formCidade = new \Application\Form\Cidade($em);
         $formUfs = new \Application\Form\Uf();
 
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+            $valores = $request->getPost();
+            var_dump($valores);exit;
+            $usuario = new \Application\Entity\Usuario();
+            $filtros = $usuario->getInputFilter();
+            $formEndereco->setInputFilter($filtros);
+            $formEndereco->setData($valores);
+            $formCidade->setInputFilter($filtros);
+            $formCidade->setData($valores);
+            $formUfs->setInputFilter($filtros);
+            $formUfs->setData($valores);
+            
+            
+            if (!$formEndereco->isValid()) {
+                $valuesEndereco = $formEndereco->getData();
+                $valuesCidade = $formCidade->getData();
+                $valuesUfs = $formUfs->getData();
+                
+                try{
+                    $ufs = $this->getService('Application\Service\Ufs')
+                            ->saveUfs($valuesUfs);
+                    $cidade = $this->getService('Application\Service\Cidade')
+                            ->saveUfs($valuesCidade);
+                    $endereco = $this->getService('Application\Service\Endereco')
+                            ->saveEndereco($valuesEndereco);
+                }catch(\Exception $e){
+                    echo $e->getMessage(); 
+                    exit;
+                }
+                return $this->redirect()->toUrl('/application/enderecos/save');    
+            }                    
+        }
+        
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if ($id > 0) {
+            $usuario = $this->getService('Application\Service\Usuario')
+                    ->findUsuario($id ,$em);
+            $form->bind($usuario);
+        }
+
         return new ViewModel(array(
             'formEndereco' => $formEndereco,
             'formUfs' => $formUfs,
             'formCidade' => $formCidade
         ));
     }
+        
+        
+        
+    
 
     public function deleteAction() {
         
