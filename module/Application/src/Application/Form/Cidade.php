af9baac39a975f18d83c9fdecf5b6f58c8c1d49f
Paulo Cella <paulocella@unochapeco.edu.br>
2015-06-08 08:39:34 -0300

    ajustes para crud de endereco

--- module/Application/src/Application/Form/Cidade.php
+++ module/Application/src/Application/Form/Cidade.php
@@ -1,21 +1,21 @@
 <?php
+
 namespace Application\Form;
 
 use Zend\Form\Element;
 use Zend\Form\Form;
 
 /**
-*
-*@category Admin
-*@package Form
-*@author Cristiano Luiz Salvagni <cristiano.salvagni@unochapeco.edu.br>
-* @license  Copyright <http://www.softwarecontracts.net/p05_copyright_patent_software.htm>
-* @link     localhost
-*/
-class Cidade extends Form
-{
-    public function __construct($em = null)
-    {
+ *
+ * @category Admin
+ * @package Form
+ * @author Cristiano Luiz Salvagni <cristiano.salvagni@unochapeco.edu.br>
+ * @license  Copyright <http://www.softwarecontracts.net/p05_copyright_patent_software.htm>
+ * @link     localhost
+ */
+class Cidade extends Form {
+
+    public function __construct($em = null) {
         parent::__construct('cidade');
         $this->setName('cidade');
         $this->setAttribute('method', 'post');
@@ -27,18 +27,23 @@ class Cidade extends Form
         ));
 
         $this->add(
-            array(
+                array(
                     'type' => 'text',
                     'name' => 'cidade',
                     'options' => array(
                         'label' => 'Cidade'
                     ),
-                ));
+                    'attributes' => array(
+                        'placeholder' => 'Informe a cidade',
+                        'id' => 'cidade',
+                        'class' => 'form-control'
+                    )
+        ));
 
-         $this->add(array(
+        $this->add(array(
             'type' => 'DoctrineModule\Form\Element\ObjectSelect',
             'name' => 'uf',
-            'options' => array(                
+            'options' => array(
                 'label' => 'UF',
                 'object_manager' => $em,
                 'target_class' => 'Application\Entity\Uf',
@@ -52,11 +57,15 @@ class Cidade extends Form
                         'orderBy' => array('descricao' => 'ASC'),
                     ),
                 ),
-            ),            
+            ),
+            'attributes' => array(
+                'placeholder' => 'Informe a UF',
+                'id' => 'uf',
+                'class' => 'form-control'
+            ),
         ));
-
-                
     }
+
 }
 
 ?>
--- module/Application/src/Application/Form/Uf.php
+++ module/Application/src/Application/Form/Uf.php
@@ -1,21 +1,21 @@
 <?php
+
 namespace Application\Form;
 
 use Zend\Form\Element;
 use Zend\Form\Form;
 
 /**
-*
-*@category Admin
-*@package Form
-*@author Cezar Junior de Souza <cezar08@unochapeco.edu.br>
-* @license  Copyright <http://www.softwarecontracts.net/p05_copyright_patent_software.htm>
-* @link     localhost
-*/
-class Uf extends Form
-{
-    public function __construct()
-    {
+ *
+ * @category Admin
+ * @package Form
+ * @author Cezar Junior de Souza <cezar08@unochapeco.edu.br>
+ * @license  Copyright <http://www.softwarecontracts.net/p05_copyright_patent_software.htm>
+ * @link     localhost
+ */
+class Uf extends Form {
+
+    public function __construct() {
         parent::__construct('uf');
         $this->setName('uf');
         $this->setAttribute('method', 'post');
@@ -27,16 +27,20 @@ class Uf extends Form
         ));
 
         $this->add(
-            array(
+                array(
                     'type' => 'text',
                     'name' => 'descricao',
                     'options' => array(
                         'label' => 'Estado'
                     ),
-                ));
-
-               
+                    'attributes' => array(
+                        'placeholder' => 'Informe o Estado',
+                        'id' => 'descricao',
+                        'class' => 'form-control'
+                    )
+        ));
     }
+
 }
 
 ?>
