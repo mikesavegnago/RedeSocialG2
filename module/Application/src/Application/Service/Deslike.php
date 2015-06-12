26899c3d8a9c67f517729206ad1b41e9c845fe9f
Paulo Cella <paulocella@unochapeco.edu.br>
2015-06-12 12:00:36 -0300

    Criado service like e deslike

---
+++ module/Application/src/Application/Service/Deslike.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Application\Service;
+
+use Core\Service\Service;
+use Core\Model\EntityException as EntityException;
+
+/**
+ * Service Save Entityes
+ *
+ * @category Application
+ * @package  Service
+ * @author   Paulo José Cella <paulocella@unochapeco.edu.br> 
+ * @link     localhost 
+ */
+class Desike extends Service {
+
+    public function saveDeslike($values) {
+        
+        $session = $this->getServiceManager()->get('Session');
+         $usuario = $session->offsetGet('user');
+         $em = $this->getObjectManager();
+         
+         $deslike = new \Application\Entity\Deslike();
+        
+         //preencher dados de like
+         
+        $this->getObjectManager()->persist($deslike);
+        try {
+            $this->getObjectManager()->flush();
+        } catch (\Exception $e) {
+            throw new EntityException('Erro ao salvar deslike' . $e);
+            exit;
+        }
+
+        return $deslike;
+    }
+    
+    public function deleteDeslike($id)
+    {
+        $deslike = $this->find($id);
+        if(!$deslike)
+           throw new EntityException('Registro não encontrado');
+        $this->getObjectManager()->remove($deslike);
+        try{
+            $this->getObjectManager()->flush();
+        }catch(\Exception $e){
+            throw new EntityException('Erro ao deletar deslike');
+        }
+    }
+
+}
---
+++ module/Application/src/Application/Service/Like.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Application\Service;
+
+use Core\Service\Service;
+use Core\Model\EntityException as EntityException;
+
+/**
+ * Service Save Entityes
+ *
+ * @category Application
+ * @package  Service
+ * @author   Paulo José Cella <paulocella@unochapeco.edu.br> 
+ * @link     localhost 
+ */
+class Like extends Service {
+
+    public function saveLike($values) {
+        
+        $session = $this->getServiceManager()->get('Session');
+         $usuario = $session->offsetGet('user');
+         $em = $this->getObjectManager();
+         
+         $like = new \Application\Entity\Like();
+        
+         //preencher dados de like
+         
+        $this->getObjectManager()->persist($like);
+        try {
+            $this->getObjectManager()->flush();
+        } catch (\Exception $e) {
+            throw new EntityException('Erro ao salvar like' . $e);
+            exit;
+        }
+
+        return $like;
+    }
+  
+    public function deleteLike($id)
+    {
+        $like = $this->find($id);
+        if(!$like)
+           throw new EntityException('Registro não encontrado');
+        $this->getObjectManager()->remove($like);
+        try{
+            $this->getObjectManager()->flush();
+        }catch(\Exception $e){
+            throw new EntityException('Erro ao deletar like');
+        }
+    }
+
+}
