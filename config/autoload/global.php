8214465e9f1669b18e737744cda60dba9166489b
Paulo Cella <paulocella@unochapeco.edu.br>
2015-06-13 02:14:31 -0300

    Acls

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Global Configuration Override
  *
@@ -10,7 +11,127 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
-
 return array(
     // ...
+
+    'acl' => array(
+        'roles' => array(
+            'VISITANTE' => null,
+            'ADMIN' => 'VISITANTE',
+        ),
+        'resources' => array(
+            'Application\Controller\Index.index',
+            'Application\Controller\Index.opcoes',
+            'Application\Controller\Index.pagina',
+            'Application\Controller\Login.login',
+            'Application\Controller\Login.logout',
+            'Application\Controller\Usuarios.index',
+            'Application\Controller\Usuarios.save',
+            'Application\Controller\Usuarios.delete',
+            'Application\Controller\Perfis.index',
+            'Application\Controller\Perfis.save',
+            'Application\Controller\Perfis.delete',
+            'Application\Controller\Murais.index',
+            'Application\Controller\Murais.save',
+            'Application\Controller\Murais.delete',
+            'Application\Controller\Comentarios.index',
+            'Application\Controller\Comentarios.save',
+            'Application\Controller\Comentarios.delete',
+            'Application\Controller\Likes.index',
+            'Application\Controller\Likes.save',
+            'Application\Controller\Likes.delete',
+            'Application\Controller\Deslikes.index',
+            'Application\Controller\Deslikes.save',
+            'Application\Controller\Deslikes.delete',
+            'Application\Controller\Imagens.index',
+            'Application\Controller\Imagens.save',
+            'Application\Controller\Imagens.delete',
+            'Application\Controller\Deslikes.delete',
+            'Application\Controller\Albuns.index',
+            'Application\Controller\Albuns.save',
+            'Application\Controller\Albuns.delete',
+            'Application\Controller\Cidades.index',
+            'Application\Controller\Cidades.save',
+            'Application\Controller\Cidades.delete',
+            'Application\Controller\Enderecos.index',
+            'Application\Controller\Enderecos.save',
+            'Application\Controller\Enderecos.delete',
+            'Application\Controller\Eventos.index',
+            'Application\Controller\Eventos.save',
+            'Application\Controller\Eventos.delete',
+            'Application\Controller\MuralEventos.index',
+            'Application\Controller\MuralEventos.save',
+            'Application\Controller\MuralEventos.delete',
+            'Application\Controller\ParticipantesEventos.index',
+            'Application\Controller\ParticipantesEventos.save',
+            'Application\Controller\ParticipantesEventos.delete',
+            'Application\Controller\Ufs.index',
+            'Application\Controller\Ufs.save',
+            'Application\Controller\Ufs.delete',
+            'Application\Controller\Usuarios.index',
+            'Application\Controller\Usuarios.save',
+            'Application\Controller\Usuarios.delete',
+        ),
+        'privilege' => array(
+            'VISITANTE' => array(
+                'allow' => array(
+                    'Application\Controller\Login.login',
+                    'Application\Controller\Login.logout'
+                )
+            ),
+            'ADMIN' => array(
+                'allow' => array(
+                    'Application\Controller\Index.index',
+                    'Application\Controller\Index.opcoes',
+                    'Application\Controller\Index.pagina',
+                    'Application\Controller\Usuarios.index',
+                    'Application\Controller\Usuarios.save',
+                    'Application\Controller\Usuarios.delete',
+                    'Application\Controller\Perfis.index',
+                    'Application\Controller\Perfis.save',
+                    'Application\Controller\Perfis.delete',
+                    'Application\Controller\Murais.index',
+                    'Application\Controller\Murais.save',
+                    'Application\Controller\Murais.delete',
+                    'Application\Controller\Comentarios.index',
+                    'Application\Controller\Comentarios.save',
+                    'Application\Controller\Comentarios.delete',
+                    'Application\Controller\Likes.index',
+                    'Application\Controller\Likes.save',
+                    'Application\Controller\Likes.delete',
+                    'Application\Controller\Deslikes.index',
+                    'Application\Controller\Deslikes.save',
+                    'Application\Controller\Deslikes.delete',
+                    'Application\Controller\Imagens.index',
+                    'Application\Controller\Imagens.save',
+                    'Application\Controller\Imagens.delete',
+                    'Application\Controller\Deslikes.delete',
+                    'Application\Controller\Albuns.index',
+                    'Application\Controller\Albuns.save',
+                    'Application\Controller\Albuns.delete',
+                    'Application\Controller\Cidades.index',
+                    'Application\Controller\Cidades.save',
+                    'Application\Controller\Cidades.delete',
+                    'Application\Controller\Enderecos.index',
+                    'Application\Controller\Enderecos.save',
+                    'Application\Controller\Enderecos.delete',
+                    'Application\Controller\Eventos.index',
+                    'Application\Controller\Eventos.save',
+                    'Application\Controller\Eventos.delete',
+                    'Application\Controller\MuralEventos.index',
+                    'Application\Controller\MuralEventos.save',
+                    'Application\Controller\MuralEventos.delete',
+                    'Application\Controller\ParticipantesEventos.index',
+                    'Application\Controller\ParticipantesEventos.save',
+                    'Application\Controller\ParticipantesEventos.delete',
+                    'Application\Controller\Ufs.index',
+                    'Application\Controller\Ufs.save',
+                    'Application\Controller\Ufs.delete',
+                    'Application\Controller\Usuarios.index',
+                    'Application\Controller\Usuarios.save',
+                    'Application\Controller\Usuarios.delete',
+                )
+            ),
+        )
+    )
 );
