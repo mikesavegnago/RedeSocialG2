4563eccc0168254d9280b609082f97b8a08943c2
Paulo Cella <paulocella@unochapeco.edu.br>
2015-06-13 09:54:49 -0300

    adicionado length

--- module/Application/src/Application/Entity/Evento.php
+++ module/Application/src/Application/Entity/Evento.php
@@ -27,7 +27,7 @@ class Evento extends Entity
     protected $id;
     
     /**
-     * @ORM\Column (type="string")
+     * @ORM\Column (type="string",  length=10000000)
      *
      * @var string
      */
